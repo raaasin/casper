caa65b0bb19289069ac0cc34166ddea2b3a02a0c
raaasin <nisarvskp@gmail.com>
2023-12-22 22:22:40 +0530

    v1.0

--- bot.py
+++ bot.py
@@ -1,25 +1,30 @@
 import discord
 from creds import token
+import os
 intents = discord.Intents.default()
-intents.on_member_join = True
+intents.members = True  
 
 client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
-    print(f"We have logged in as {client.user}")
+    print(f"{client.user} is online!")
 
 @client.event
 async def on_member_join(member):
-    channel = member.guild.system_channel  # Fetching the system channel
+    channel = member.guild.system_channel  
     if channel is not None:
         embed = discord.Embed(
             title=f"Welcome {member.display_name}!",
-            description="Welcome to our Discord server! We're excited to have you here.",
+            description="Welcome to the Frost Hacks server, We're excited to have you here!",
             color=discord.Color.green()
         )
-        embed.set_thumbnail(url=member.avatar_url)
-        await channel.send(embed=embed)
+        gif_path = "./gif.gif"  
+        if os.path.exists(gif_path):
+            embed.set_thumbnail(url="attachment://" + os.path.basename(gif_path))
+            await channel.send(embed=embed, file=discord.File(gif_path, filename=os.path.basename(gif_path)))
+        else:
+            await channel.send("GIF not found.")
 
-# Replace 'YOUR_TOKEN_HERE' with your bot's token
+
 client.run(token)
